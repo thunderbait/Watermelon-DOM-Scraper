fc65630a62fb0f969e77db2bac2cb5c0c94cd2f0
Giorgos Andreou <gandreou@alacrityfoundation.com>
2020-02-13 16:06:40 +0000

    added financing

--- index.php
+++ index.php
@@ -53,21 +53,31 @@
 			$content = $dom->find('#content')[0];
 
 			$title = $content->find('.title')[0]->plaintext;
+
+			// History
 			$history = $content->find('h2')[1];
 			$historyTitle = $content->find('h2')[1]->plaintext;
 			$historyText = $history->next_sibling()->plaintext;
 
+			//aims
 			$aims = $content->find('h2')[2];
 			$aimsTitle = $content->find('h2')[2]->plaintext;
 			$aimsText = $aims->next_sibling()->plaintext;
 
+			//events
 			$events = $content->find('h2')[3];
 			$eventsTitle = $content->find('h2')[3]->plaintext;
 			$eventsText = $events->next_sibling()->plaintext;
 
+			// structure
 			$structure = $content->find('h2')[4];
 			$structureTitle = $content->find('h2')[4]->plaintext;
 			$structureText = $structure->next_sibling()->plaintext;
+
+			//financing
+			$financing = $content->find('h2')[5];
+			$financingTitle = $content->find('h2')[5]->plaintext;
+			$financingText = $financing->next_sibling()->plaintext;
 
 
 
@@ -79,6 +89,8 @@
 			echo $aimsText . "<br>";
 			echo $events . "<br>";
 			echo $eventsText . "<br>";
+			echo $financing . "<br>";
+			echo $financingText . "<br>";
 
 
 			?>
